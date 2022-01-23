c2b0d18c786665ad65b39ac554f45615e24de25e
Luciano Berchon <berchon@gmail.com>
2022-01-22 22:52:08 -0300

    Implemented data persistence

---
+++ Marvel App/Model/Characters/FavoriteModel.swift
@@ -0,0 +1,8 @@
+//
+//  favoriteModel.swift
+//  Marvel App
+//
+//  Created by Luciano Berchon on 22/01/22.
+//
+
+import Foundation
---
+++ Marvel App/Services/ManagerStorage.swift
@@ -0,0 +1,8 @@
+//
+//  ManagerStorage.swift
+//  Marvel App
+//
+//  Created by Luciano Berchon on 21/01/22.
+//
+
+import Foundation
